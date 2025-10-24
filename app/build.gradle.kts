fd0561ab8106fc490a712cd2e388135cd1de10c5
jeferson01jeferson02-ux <jeferson01jeferson02@gmail.com>
2025-10-23 23:46:59 -0300

    Atualizando versão para 1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.iptvservidor"
         minSdk = 21
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.1"
+        versionCode 1
+        versionName "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
